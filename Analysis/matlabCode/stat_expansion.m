de5d7fe3d2569309a5f36ee4d38d7c4287b10215
ShouwenMa <150151690+ShouwenMa@users.noreply.github.com>
2025-08-19 16:39:26 +0200

    Update stat_expansion.m

--- Analysis/matlabCode/stat_expansion.m
+++ Analysis/matlabCode/stat_expansion.m
@@ -45,3 +45,68 @@ legend({'Center','Edge','Corner'},'FontSize',12)
 view([ 185.5   22])
 
 clearvars i j
+
+
+
+
+
+
+
+%%% calculate the difference between pairwise distances
+
+levRLS = unique(pd_cell(:,1)); % size
+levRLR = unique(pd_cell(:,2)); % growth rate
+levRLA = unique(pd_cell(:,3)); % angle
+nlevRLS = length(levRLS);
+nlevRLR = length(levRLR);
+nlevRLA = length(levRLA);
+
+levIS = unique(pd_DPI(:,1)); % initial size (um)
+nlevIS = length(levIS);
+levER = unique(pd_DPI(:,2)); % expansion rate = 1 + p/100 (p: % expansion)
+nlevER = length(levER);
+levIL = unique(pd_DPI(:,3)); % initial locaition, 1.center, 2.edge. 3.corner
+nlevIL = length(levIL);
+
+i = 1;ii=10;iii=2;
+
+statRLoc = [];
+for i = 1:nlevIS
+    pd_cell_i = pd_cell(find(pd_cell(:,1)==levIS(i)),:);
+    levERi = unique(pd_cell_i(:,2)); % growth rate
+    nlevERi = length(levERi);
+    levILi = unique(pd_cell_i(:,3)); % angle
+    nlevILi = length(levILi);
+    pd_cell_i_0 = pd_cell_i(find(pd_cell_i(:,2)==levERi(1)),:); % growth rate = 1
+    for ii = 2:nlevERi
+        for iii = 1:nlevILi
+            pd_cell_ii_iii = pd_cell_i(find(pd_cell_i(:,2)==levERi(ii) & ...
+                pd_cell_i(:,3)==levILi(iii)),:);
+            pd_cell_ii_iii_0 = pd_cell_i_0(find(pd_cell_i_0(:,3)==levILi(iii)),:);
+            diff_ii_iii = pd_cell_ii_iii(:,4)-pd_cell_ii_iii_0(:,4);
+
+            statRLociii = [levIS(i) levERi(ii) levILi(iii) mean(diff_ii_iii) std(diff_ii_iii)];
+            statRLoc=[statRLoc;statRLociii];
+
+        end
+    end
+end
+
+%%%%%%%%% Identical cell displacements despite using different initial locations
+figure 
+statRLoc1 = statRLoc(find(statRLoc(:,3)==1),:);
+scatter3(statRLoc1(:,1),(statRLoc1(:,2)-1)*100,statRLoc1(:,4),10)
+hold on
+statRLoc1 = statRLoc(find(statRLoc(:,3)==2),:);
+scatter3(statRLoc(:,1),(statRLoc(:,2)-1)*100,statRLoc(:,4),20)
+statRLoc1 = statRLoc(find(statRLoc(:,3)==3),:);
+scatter3(statRLoc1(:,1),(statRLoc1(:,2)-1)*100,statRLoc1(:,4),30)
+hold off
+xlabel('Initial size (\mum)','FontSize',12)
+ylabel('% expansion','FontSize',12)
+zlabel('Mean displacement (\mum)','FontSize',12)
+legend({'Center','Edge','Corner'},'FontSize',12)
+set(gca,'TickDir','out','Box','off')
+set(gca,'TickLength',[0.02, 0.01])
+% view(90,0)
+view([ 185.5   22])
